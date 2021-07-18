56f656273635cedb5820b06ee0ed07e8c1ef14f1
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-07-18 14:58:41 +0100

    Store user tokens rather than their deck

--- src/handlers/matchmaking-listener.ts
+++ src/handlers/matchmaking-listener.ts
@@ -11,32 +11,40 @@ export class MatchmakingListener extends GameEventListener {
   ];
 
   matchmakingHandler(user: User, gameRoom: GameRoom, args: any[]): void {
-      console.log("matchmaking: received command");
-      if (args.length != 2) {
-          user.socket.emit('message', 'matchmaking: Incorrect arguments');
-          return;
-      }
+    console.log("matchmaking: received command");
+    // Expecting an event of -> matchmaking: username, jwt, access token
+    if (args.length != 3) {
+      user.socket.emit("message", "matchmaking: Incorrect arguments");
+      return;
+    }
 
-      const userName: string = args[0];
-      const deckIds: number[] = args[1];
+    const userName: string = args[0];
+    const jwtToken: string = args[1];
+    const accessToken: string = args[2];
 
-      if (userName === null || userName === undefined) {
-          user.socket.emit('message', 'matchmaking: Unable to set username');
-          return;
-      }
+    if (userName === null || userName === undefined) {
+      user.socket.emit("message", "matchmaking: Unable to set username");
+      return;
+    }
 
-      if (deckIds === null || deckIds === undefined) {
-        user.socket.emit('message', 'matchmaking: Unable to set deck');
-        return;
+    if (jwtToken === null || jwtToken === undefined) {
+      user.socket.emit("message", "matchmaking: Unable to set JWT Token");
+      return;
+    }
+
+    if (accessToken === null || accessToken === undefined) {
+      user.socket.emit("message", "matchmaking: Unable to set Access Token");
+      return;
     }
 
     user.name = userName;
-    user.deck = deckIds;
+    user.jwtToken = jwtToken;
+    user.accessToken = accessToken;
 
-      // Is this room ready to play?
-      if (gameRoom.users.length == 2) {
-          console.log(`Sending ready event to room ${gameRoom.name}`);
-          gameRoom.socket.emit('ready');
-      }
+    // Is this room ready to play?
+    if (gameRoom.users.length == 2) {
+      console.log(`Sending ready event to room ${gameRoom.name}`);
+      gameRoom.socket.emit("ready");
+    }
   }
 }
