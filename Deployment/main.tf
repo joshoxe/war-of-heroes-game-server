72fe9bb7b0d201974914795e345e2dabe29bda0a
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-07-23 16:19:49 +0100

    Add needed reserved bool

--- Deployment/main.tf
+++ Deployment/main.tf
@@ -30,6 +30,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     kind = "Linux"
+    reserved = true
 
     sku {
         tier = "Shared"
