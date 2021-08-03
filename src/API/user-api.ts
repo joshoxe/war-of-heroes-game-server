9816f15923f0b3849847004f2629455a9aa19dc9
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-03 22:28:34 +0100

    Add class for managing user API interactions

---
+++ src/API/user-api.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+require('ssl-root-cas').inject();
+import * as https from 'https';
+import { User } from '../user';
+
+export class UserApi {
+    userApiUrl: string = "https://localhost:44328";
+
+    recordWin(user: User, coinsForWin: number) {
+        const headers = {
+            'Content-Type': "application/json",
+            'Authorization': `Bearer ${user.jwtToken}`,
+            'AccessToken': user.accessToken
+        }
+        const winEndpoint = `/user/${user.id}/win`
+
+        axios.put(this.userApiUrl + winEndpoint, `${coinsForWin}`,
+            {
+                headers: headers,
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                  })
+            })
+            .catch((error) => {
+                console.log("Unable to record win:", error);
+            })
+    }
+
+    recordLoss(user: User, coinsForLoss: number) {
+        const headers = {
+            'Content-Type': "application/json",
+            'Authorization': `Bearer ${user.jwtToken}`,
+            'AccessToken': user.accessToken
+        }
+        const lossEndpoint = `/user/${user.id}/loss`
+
+        axios.put(this.userApiUrl + lossEndpoint, `${coinsForLoss}`,
+            {
+                headers: headers,
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                  })
+            })
+            .catch((error) => {
+                console.log("Unable to record loss:", error);
+            })
+    }
+}
