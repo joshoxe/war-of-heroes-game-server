810f49dee765f7f98ac6b992651d99a2f90527b9
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-15 17:47:48 +0100

    Add a class for interacting with the Hero API

---
+++ src/API/hero-api.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import * as https from 'https';
+import { Hero } from "../hero";
+import { User } from "../user";
+
+export class HeroApi {
+    heroApiUrl: string = "https://warofheroesheroes.azurewebsites.net";
+
+    async getHeroes(user: User, deck: number[]): Promise<Hero[]> {
+        const headers = {
+            'Content-Type': "application/json",
+            'Authorization': `Bearer ${user.jwtToken}`,
+            'AccessToken': user.accessToken
+        }
+        const heroEndpoint = '/hero/ids'
+        var deckHeroes: Hero[];
+
+        await axios.post(this.heroApiUrl + heroEndpoint, {"heroIds": deck},
+            {
+                headers: headers,
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false
+                  })
+            })
+            .then((heroes) => {
+                deckHeroes = heroes.data;
+            }) 
+            .catch((error) => {
+                console.log("Unable to get heroes:", error);
+            })
+
+        return deckHeroes;
+    }
+}
