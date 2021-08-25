3869555dcae0593e82bb182979ac07432cf010db
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-25 17:24:52 +0100

    Add new gameplay events

--- src/handlers/game-listener.ts
+++ src/handlers/game-listener.ts
@@ -1,41 +1,61 @@
+import { Game } from "../game";
 import { UserApi } from "../API/user-api";
 import { GameRoom } from "../game-room";
 import { User } from "../user";
 import { GameEventListener } from "./game-event-listener";
+import { Hero } from "../hero/hero";
+import { Player } from "../player";
+import { AbilityHandler } from "../abilities/ability-handler";
+import { AbilityEvent } from "../abilities/ability-event";
 
 export class GameListener extends GameEventListener {
-    eventHandlers: Handler[] = [
-        {
-            event: "win",
-            handler: this.winHandler
-        }
-    ];
+  eventHandlers: Handler[] = [
+    {
+      event: "heroPlayed",
+      handler: this.heroPlayedHandler,
+    },
+    {
+      event: "win",
+      handler: this.winHandler,
+    },
+  ];
 
-    userApi: UserApi;
-    
-    winHandler(user: User, gameRoom: GameRoom, args: any[]) {
-        console.log("Got a winner!");
+  userApi: UserApi;
 
-        // Add win to winners record
-        this.userApi = new UserApi();
+  heroPlayedHandler(user: User, game: Game, gameObject: any[]) {
+    const container = gameObject[0];
+    const hero: Hero = gameObject[1];
+    user.socket.to(game.gameRoom.name).emit("opponentHeroPlayed", { container: container, hero: hero });
 
-        // TODO: In eventual game class, store coins literal as const
-        this.userApi.recordWin(user, 50);
+    game.currentTurnPlayer.discardPile.push(hero);
+    game.currentTurnPlayer.removeFromHand(hero);
 
-        // Add loss for everyone else in the room
-        for (let i = 0; i < gameRoom.users.length; i++) {
-            const gameRoomUser = gameRoom.users[i];
+    const abilityHandler = new AbilityHandler();
+    const event: AbilityEvent = abilityHandler.execute(game.getCurrentNonPlayer(), hero);
+    game.updatePlayer(event.player);
+    var playerObj = game.getPlayerData(event.player);
+    game.gameRoom.socket.emit(event.event, playerObj);
+    game.increaseCurrentPlays();
+  }
 
-            if (gameRoomUser === user) {
-                continue;
-            }
-            // TODO: As above
-            this.userApi.recordLoss(gameRoomUser, 5);
-        }
+  winHandler(user: User, game: Game, args: any[]) {
+    console.log("Got a winner!");
 
-        // Tell the client they won
-        user.socket.emit("win");
-        // Tell the opponent they lost
-        user.socket.to(gameRoom.name).emit("lose");
+    // Add win to winners record
+    this.userApi = new UserApi();
+
+    // TODO: In eventual game class, store coins literal as const
+    this.userApi.recordWin(user, 50);
+
+    // Add loss for everyone else in the room
+    for (let i = 0; i < game.gameRoom.users.length; i++) {
+      const gameRoomUser = game.gameRoom.users[i];
+
+      if (gameRoomUser === user) {
+        continue;
+      }
+      // TODO: As above
+      this.userApi.recordLoss(gameRoomUser, 5);
     }
+  }
 }
